cbcc9646859db4cb4f2e56f01c44681d90a22c82
khoitm <khoitm@smartosc.com>
2025-05-06 17:58:33 +0700

    add filter

--- day8/spark-etl/main.py
+++ day8/spark-etl/main.py
@@ -4,6 +4,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, floor, months_between, current_date, upper
 from dotenv import load_dotenv
 import os
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, TimestampType
 
 load_dotenv()
 
@@ -14,7 +15,7 @@ data_set_table_id = os.getenv('BIG_QUERY_TABLE_ID')
 
 def spark_transform():
     # Ask for enter min value
-    age_filter_input = input("Enter min age to filter: ")
+    age_filter_input = 30 #input("Enter min age to filter: ")
     try:
         age_filter = int(age_filter_input)
     except ValueError:
@@ -26,16 +27,52 @@ def spark_transform():
         .appName("Simple ETL Pipeline") \
         .getOrCreate()
 
+    spark.sparkContext.setLogLevel("ERROR")
+
+    # Index,User Id,First Name,Last Name,Sex,Email,Phone,Date of birth,Job Title
+    schema = StructType([
+        StructField("Index", IntegerType(), False),
+        StructField("User Id", StringType(), False),
+        StructField("First Name", StringType(), True),
+        StructField("Last Name", StringType(), True),
+        StructField("Sex", StringType(), True),
+        StructField("Email", StringType(), True),
+        StructField("Phone", StringType(), True),
+        StructField("Date of birth", TimestampType(), True),
+        StructField("Job Title", StringType(), True)
+    ])
+
     # Step 2: Extract - Read CSV into a DataFrame
-    df = spark.read.csv("people.csv", header=True, inferSchema=True)
+    df = spark.read.csv("people.csv", header=True, schema=schema)
+
+    age = floor(months_between(current_date(), col("Date of birth")) / 12)
 
     # Step 3: Transform - Filter people older than 30 and uppercase city names
-    transformed_df = df.filter(floor(months_between(current_date(), col("Date of birth")) / 12) > age_filter) \
-                    .withColumn("Job Title", upper(col("Job Title")))\
+    transformed_df = df.withColumn("Job Title", upper(col("Job Title"))) \
+                    .withColumn("Age", age) \
                     .orderBy(col("Index"))
 
+    print(f"Original row count: {transformed_df.count()}")
     # Step 4: Load - Write the result to a Parquet file
     transformed_df.write.mode("overwrite").parquet("output/people_over_30.parquet")
+
+    print('-----Result-------')
+    transformed_df.show(10)
+
+    print('----- Filter result with Male only and older 30 -------')
+    # gender_filter_df = transformed_df.filter(col("Sex") == "Male") \
+    #                     .filter(age > age_filter) \                       
+    #                     .orderBy(col("Index"))
+    gender_filter_df = transformed_df.filter(col("Sex") == "Male") \
+                        .filter(age > age_filter) \
+                        .orderBy(col("Index"))
+    gender_filter_df.show(10)
+
+    # Group by calculated age and count
+    print('----- Group by calculated age  -------')
+    grouped_df = transformed_df.groupBy(age.alias("Age")).count().orderBy("Age")
+    
+    grouped_df.show()
 
     #Save to CSV
     csv_output_path = f"output/people_over_{age_filter}.csv"
